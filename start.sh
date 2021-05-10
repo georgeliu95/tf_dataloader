e22ec6ba59d0a02fd8116d594f936319140dd658
George Liu <georgel@nvidia.com>
2021-05-10 12:50:59 +0800

    minor change

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 cd docker
 docker build -t tf-dataloader:v1 .
 cd ..
-nvidia-docker run -it --name georgel-tf --rm -v $PWD:/home tf-dataloader:v1 /bin/bash
+usr_id=`id | grep -oE "uid=[0-9]+" | grep -oE "[0-9]+"`
+nvidia-docker run -it --name georgel-tf --rm -v $PWD:/home --user ${usr_id} tf-dataloader:v1 /bin/bash
