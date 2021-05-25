60697b0b0641280a64a6b7659a7d1af2d814b21d
George Liu <georgel@nvidia.com>
2021-05-25 15:46:28 +0800

    change profile.sh

--- profile.sh
+++ profile.sh
@@ -3,10 +3,12 @@ python_file=$1
 nsys_cmd="nsys profile --sample=none --backtrace=none --cpuctxsw=none -f true"
 # echo ${python_file}
 
-for idx in {0..6}
+for idx in {1..7}
 do
     prefix=${python_file%%/*}
     profile_file="profile/${prefix}-${idx}"
     # echo "${nsys_cmd} -o ${profile_file} python ${python_file} ${idx}"
     ${nsys_cmd} -o ${profile_file} python ${python_file} ${idx}
+    # ${nsys_cmd} -o ${profile_file} horovodrun -np 4 python ${python_file} ${idx}
+    # horovodrun -np 4 python ${python_file} ${idx}
 done
